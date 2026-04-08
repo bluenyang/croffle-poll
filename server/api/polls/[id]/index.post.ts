bad3b63e068918dcd1261cb86e1e2336260a3b46
BlueNyang <xpsj20@gmail.com>
2026-04-09 02:09:20 +0900

    fix: Custom option에 voting이 되지 않던 문제 해결

--- server/api/polls/[id]/index.post.ts
+++ server/api/polls/[id]/index.post.ts
@@ -22,35 +22,42 @@ export default defineEventHandler(async (event) => {
     };
     const userId = user.id;
 
+    const [poll] = await tx.select().from(polls).where(eq(polls.id, pollId));
+    if (!poll) {
+      throw createError({ statusCode: 404 });
+    }
+
     // 커스텀 항목(직접 작성)이 넘어왔다면 먼저 option을 INSERT
     const customOptionIds: number[] = [];
 
-    if (body.customOptionValue && body.customOptionValue.length > 0) {
-      body.customOptionValue.map(async (v) => {
-        const [existingOption] = await tx
-          .select()
-          .from(pollOptions)
-          .where(and(eq(pollOptions.pollId, pollId), eq(pollOptions.value, v)));
+    if (poll.allowCustomOptions && body.customOptionValue && body.customOptionValue.length > 0) {
+      await Promise.all(
+        body.customOptionValue.map(async (v) => {
+          const [existingOption] = await tx
+            .select()
+            .from(pollOptions)
+            .where(and(eq(pollOptions.pollId, pollId), eq(pollOptions.value, v)));
 
-        if (existingOption) {
-          customOptionIds.push(existingOption.id); // 이미 존재하는 항목이면 그걸 사용
-        } else {
-          const [newOption] = await tx
-            .insert(pollOptions)
-            .values({
-              pollId,
-              value: v,
-              createdBy: userId, // 누가 추가한 항목인지 기록
-            })
-            .returning();
+          if (existingOption) {
+            customOptionIds.push(existingOption.id); // 이미 존재하는 항목이면 그걸 사용
+          } else {
+            const [newOption] = await tx
+              .insert(pollOptions)
+              .values({
+                pollId,
+                value: v,
+                createdBy: userId, // 누가 추가한 항목인지 기록
+              })
+              .returning();
 
-          if (!newOption) {
-            throw new Error('커스텀 옵션 생성 실패');
+            if (!newOption) {
+              throw new Error('커스텀 옵션 생성 실패');
+            }
+
+            customOptionIds.push(newOption.id);
           }
-
-          customOptionIds.push(newOption.id);
-        }
-      });
+        })
+      );
     }
 
     // 선택한 optionId 리스트 취합
@@ -58,22 +65,18 @@ export default defineEventHandler(async (event) => {
     if (customOptionIds.length > 0) targetOptionIds.push(...customOptionIds);
 
     // 투표 응답 인서트
-    // TODO: 중복 투표 방지 로직 (이미 이 pollId와 userId로 기록이 있는지 체크) 추가 필요
     if (targetOptionIds.length > 0) {
-      const existingPolls = await tx
-        .select()
-        .from(pollResponses)
+      // 만약 이미 투표한 기록이 있다면 삭제
+      await tx
+        .delete(pollResponses)
         .where(and(eq(pollResponses.pollId, pollId), eq(pollResponses.userId, userId)));
 
-      const polldOptionIds = existingPolls.map((v) => v.optionId);
-
-      const newResponsesToInsert = targetOptionIds
-        .filter((optId: number) => !polldOptionIds.includes(optId))
-        .map((optId: number) => ({
-          pollId,
-          optionId: optId,
-          userId: userId,
-        }));
+      // 새로운 투표 기록 인서트
+      const newResponsesToInsert = targetOptionIds.map((optId: number) => ({
+        pollId,
+        optionId: optId,
+        userId: userId,
+      }));
 
       if (newResponsesToInsert.length > 0) {
         await tx.insert(pollResponses).values(newResponsesToInsert);
